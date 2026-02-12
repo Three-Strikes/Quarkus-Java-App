7b40720db02f0478e3ddcf509eb37a957f9c19f0
Rafael Cardoso <1190982@isep.ipp.pt>
2026-02-12 20:52:48 +0000

    Added API Documentation and CORS config.

--- src/main/java/com/controllers/LabSeqController.java
+++ src/main/java/com/controllers/LabSeqController.java
@@ -5,6 +5,11 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import java.math.BigInteger;
 
@@ -17,6 +22,17 @@ public class LabSeqController {
     @Path("{n}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Operation(summary = "Get the result of the labseq sequence for an n number.", description = "Returns the result of the sequence L(n) = L(n-4) + L(n-3).")
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Returns the result of the labseq sequence."
+            ),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "The inserted parameter was not able to be processed."
+            )
+    })
     public BigInteger CalculateLabSeqSequence(int n) {
         try{
             return serv.calculateLabSeq(n);
